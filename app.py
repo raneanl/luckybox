cb75d1639749f8ca010738cd369d78033b523640
raneanl <dlswns68@gmail.com>
2025-04-24 15:33:32 +0000

    🎉 초기 커밋: 럭키박스 게임

--- app.py
+++ app.py
@@ -5,13 +5,11 @@ import random
 app = Flask(__name__)
 
 # 선물 번호 지정 (예: 3개만 당첨)
-def generate_gift_numbers():
-    chosen = random.sample(range(1, 31), 3)
-    return {
-        chosen[0]: {"name": "고화질 방셀(1475)", "image": "1475.png"},
-        chosen[1]: {"name": "치킨 기프티콘", "image": "치킨.png"},
-        chosen[2]: {"name": "스타벅스 쿠폰", "image": "starbucks.png"},
-    }
+gift_numbers = {
+    3: {"name": "스타벅스 기프티콘", "image": "starbucks.png"},
+    13: {"name": "치킨 기프티콘", "image": "치킨.png"},
+    28: {"name": "고화질 방셀(1475)", "image": "1475.png"},
+}
 
 # 이미 선택된 번호 추적
 used_numbers = set()
@@ -33,7 +31,6 @@ def pick():
     # 모든 번호 사용 → 초기화 + 새 당첨 번호 생성
     if len(used_numbers) >= 30:
         used_numbers.clear()
-        gift_numbers = generate_gift_numbers()
         return jsonify({"status": "reset"})
 
     prize = gift_numbers.get(number)
